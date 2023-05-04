e8f99c65abd4b1f6ed29f4dced49f12d3d979c5c
Atud Jerry <atudjerry33@gmail.com>
2023-05-04 23:40:37 +0100

    dropdown menu with javaScript file added

---
+++ Script.js
@@ -0,0 +1,8 @@
+ const button = document.getElementById("but")
+ const dropdown = document.querySelector(".dropdown-list")
+
+ button.addEventListener("click", function(){
+    dropdown.classList.toggle("close");
+
+ })
+ 
